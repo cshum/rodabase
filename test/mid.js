e33aecea79d46adbbd76d67e4bcc684cd91abc4a
Adrian Shum <me@cshum.com>
2015-03-26 14:30:05 +0800

    Tweak mid test case

--- test/mid.js
+++ test/mid.js
@@ -1,7 +1,7 @@
 var tape = require('tape');
 var mid = require('../lib/mid');
 
-tape('Node ID', function(t){
+tape('MID', function(t){
   var test = 'test/data/mid';
   var test2 = 'test/data/mid2';
   var id = mid(test);
@@ -11,5 +11,7 @@ tape('Node ID', function(t){
   t.ok(id2 === mid(test2), 'test2 === id2');
   t.ok(id !== id2, 'test !== test2');
   t.ok(mid(test) !== mid(test2), 'test !== test2');
+  t.equal(id.length, 8, 'id length 8');
+  t.equal(id2.length, 8, 'id2 length 8');
   t.end();
 });
