5635fa2486eed7140c5ded8d0f251d6043e4ca1c
Adrian Shum <me@cshum.com>
2015-03-31 21:34:02 +0800

    ADD util encode/decode

---
+++ test/util.js
@@ -0,0 +1,13 @@
+var tape = require('tape');
+var util = require('../lib/util');
+
+tape('en/decode Identity', function(t){
+  function I(source){
+    return util.decode(util.encode(source));
+  }
+  for(var i = 0; i< 10;i++){
+    var v = Math.random();
+    t.equal(v, I(v));
+  }
+  t.end();
+});
