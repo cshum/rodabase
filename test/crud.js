85ecbccbdb6b96550d5336b1d473a8dea74e5a13
Adrian Shum <me@cshum.com>
2015-04-01 15:52:54 +0800

    ADD tx put test case

--- test/crud.js
+++ test/crud.js
@@ -8,8 +8,34 @@ var roda = rodabase('./test/data/crud.json', {
   // db: memdown
   db: jsondown
 });
-
 var n = 100;
+
+tape('Put tx increment', function(t){
+  var api = roda('t1');
+  // api.use('diff', function(ctx, next){
+  //   setTimeout(next, 50);
+  // });
+  t.plan(n + 2);
+  function run(i){
+    var tx = roda.transaction();
+    api.get('k', tx, function(err, val){
+      console.log(val);
+      api.put('k', {k: (val ? val.k : 0) + 1}, tx);
+      tx.commit(function(err){
+        t.notOk(err,'committed '+ i);
+        if(i === n - 1){
+          api.get('k', function(err, val){
+           t.ok(val);
+           t.equal(val.k, n, 'Tx incrememnt');
+          });
+        }
+      });
+    });
+  }
+  for(var i = 0; i < n; i++)
+    run(i);
+});
+
 tape('Put '+ n +' increment', function(t){
   t.plan(n + 2);
   var _id = '', _rev = '';
@@ -18,7 +44,7 @@ tape('Put '+ n +' increment', function(t){
   //   setTimeout(next, 100);
   // });
   for(var i = 0; i < n; i++){
-    roda('test').put({
+    roda('t2').put({
       i: i
     }, function(err, val){
       t.ok(!err && val, 'no error');
