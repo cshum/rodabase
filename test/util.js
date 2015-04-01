e0d6e1c519ad7dd1b26622ffae74d029811915a9
Adrian Shum <me@cshum.com>
2015-04-01 21:48:39 +0800

    Tweak _rev generation

--- test/util.js
+++ test/util.js
@@ -5,9 +5,21 @@ tape('Identity', function(t){
   function I(source){
     return util.decode(util.encode(source));
   }
-  for(var i = 0; i < 10;i++){
+  for(var i = 0; i < 1000;i++){
     var v = Math.random();
     t.equal(v, I(v));
   }
+  t.end();
+});
+
+tape('lexicographical', function(t){
+  var ok = true;
+  var prev = util.encode(0);
+  for(var i = 1; i < 1000; i++){
+    var curr = util.encode(i);
+    ok &= curr > prev;
+    prev = curr;
+  }
+  t.ok(ok);
   t.end();
 });
