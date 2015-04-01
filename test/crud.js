87631a69bb4d19bd8c970c2de312269f7d71433c
Adrian Shum <me@cshum.com>
2015-04-01 14:06:06 +0800

    crud test case

--- test/crud.js
+++ test/crud.js
@@ -2,23 +2,29 @@ var tape = require('tape');
 var rodabase = require('../');
 var memdown = require('memdown');
 var jsondown = require('jsondown');
+var _ = require('underscore');
 
 var roda = rodabase('./test/data/crud.json', {
   // db: memdown
   db: jsondown
 });
 
-tape('Put 10 increments', function(t){
-  t.plan(2 * 10);
+tape('Put 10 increment', function(t){
+  t.plan(2 * 10 + 2);
   var _id = '', _rev = '';
-  for(var i = 0; i < 10; i++){
-    roda('test2').put({
-      i: Math.random()
+  var list = [];
+  for(var i = 0; i < 100; i++){
+    roda('test').put({
+      i: i
     }, function(err, val){
-      t.ok(val._id > _id, '_id increments');
-      t.ok(val._rev > _rev, '_rev increments');
-      _id = val._id;
-      _rev = val._rev;
+      t.notOk(err, 'no error');
+      t.ok(val, 'has value');
+      list.push(val);
+      if(val.i === 9){
+        list = _.sortBy(list, 'i');
+        t.deepEqual(_.sortBy(list, '_id'), list, '_id incremental');
+        t.deepEqual(_.sortBy(list, '_rev'), list, '_rev incremental');
+      }
     });
   }
 });
