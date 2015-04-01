7ba84cf95d3cb7e807617b8e90fad2f118d5aa39
Adrian Shum <me@cshum.com>
2015-04-01 14:19:14 +0800

    Tweak test case

--- test/crud.js
+++ test/crud.js
@@ -9,18 +9,22 @@ var roda = rodabase('./test/data/crud.json', {
   db: jsondown
 });
 
-tape('Put 10 increment', function(t){
-  t.plan(2 * 10 + 2);
+var n = 10;
+tape('Put '+ n +' increment', function(t){
+  t.plan(2 * n + 2);
   var _id = '', _rev = '';
   var list = [];
-  for(var i = 0; i < 100; i++){
+  // roda('test').use('diff', function(ctx, next){
+  //   setTimeout(next, 100);
+  // });
+  for(var i = 0; i < n; i++){
     roda('test').put({
       i: i
     }, function(err, val){
       t.notOk(err, 'no error');
       t.ok(val, 'has value');
       list.push(val);
-      if(val.i === 9){
+      if(val.i === n - 1){
         list = _.sortBy(list, 'i');
         t.deepEqual(_.sortBy(list, '_id'), list, '_id incremental');
         t.deepEqual(_.sortBy(list, '_rev'), list, '_rev incremental');
