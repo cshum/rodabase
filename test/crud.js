ebee950f1cbce6fcb8487ae9585db7edc1380745
Adrian Shum <me@cshum.com>
2015-04-04 15:24:09 +0800

    ADD tx increment test case

--- test/crud.js
+++ test/crud.js
@@ -10,13 +10,13 @@ var roda = rodabase('./test/data/crud.json', {
 });
 var n = 100;
 
-roda.base.use('diff', function delay(ctx, next){
-  console.log(ctx.result);
-  setTimeout(next, 100);
-});
+// roda.base.use('diff', function delay(ctx, next){
+//   console.log(ctx.result);
+//   setTimeout(next, 100);
+// });
 
-tape('Put tx read lock', function(t){
-  var api = roda('t1');
+tape('Read lock', function(t){
+  var api = roda('1');
 
   t.plan(2);
   function run(i){
@@ -46,18 +46,38 @@ tape('Put tx read lock', function(t){
 
 tape('Put increment', function(t){
   t.plan(2);
-  var api = roda('t2');
-  var _id = '', _rev = '';
-  var list = [];
+  var api = roda('2');
 
   for(var i = 0; i < n; i++)
     api.put({ i: i }, function(err, val){
-      list.push(val);
       if(val.i === n - 1){
-        list = _.sortBy(list, 'i');
-        t.deepEqual(_.sortBy(list, '_id'), list, '_id incremental');
-        t.deepEqual(_.sortBy(list, '_rev'), list, '_rev incremental');
+        api.read(function(err, list){
+          list = _.sortBy(list, 'i');
+          t.deepEqual(_.sortBy(list, '_id'), list, '_id incremental');
+          t.deepEqual(_.sortBy(list, '_rev'), list, '_rev incremental');
+        });
       }
     });
+});
+
+tape('Put tx increment', function(t){
+  t.plan(4);
+  var api = roda('3');
+  var tx = roda.transaction();
+
+  for(var i = 0; i < n; i++)
+    api.put({ i: i }, tx);
+    
+  api.read(function(err, list){
+    t.equal(list.length, 0, 'list empty before commit');
+    tx.commit(function(){
+      api.read(function(err, list){
+        list = _.sortBy(list, 'i');
+        t.equal(list.length, n, 'list filled after commit');
+        t.deepEqual(_.sortBy(list, '_id'), list, '_id incremental');
+        t.deepEqual(_.sortBy(list, '_rev'), list, '_rev incremental');
+      });
+    });
+  });
 });
 
