d6b97b14d8612d44270a1ef5f2cf0514e7f0b6b3
Adrian Shum <me@cshum.com>
2015-04-08 11:33:33 +0800

    ADD util.clockObject

--- test/util.js
+++ test/util.js
@@ -2,19 +2,20 @@ var tape = require('tape');
 var util = require('../lib/util');
 
 tape('encode decode', function(t){
-  var ok = true;
+  var lex = true;
+  var id = true;
   var m = 1000000;
   var em = util.encode(m);
 
   for(var i = 1; i < 1000; i++){
     var n = Math.random() * m;
     var en = util.encode(n);
-    //lexicographical 
-    ok &= (n >= m && en >= em) || (n < m && en < em);
-    //identical
-    ok &= n === util.decode(en);
+
+    lex &= (n >= m && en >= em) || (n < m && en < em);
+    id &= n === util.decode(en);
   }
-  t.ok(ok);
+  t.ok(lex, 'lexicographical');
+  t.ok(id, 'identical');
   t.end();
 });
 
