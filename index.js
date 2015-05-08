154eb4c4541111601174a7d878aa6d292a981a5e
Adrian Shum <me@cshum.com>
2015-05-08 23:46:02 +0800

    Tweak shim

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+//shim
+if(process.browser){
+  require("indexeddbshim");
+  require("setimmediate");
+}
+
+if (!Object.is) {
+  Object.is = function(v1, v2) {
+    if (v1 === 0 && v2 === 0) {
+      return 1 / v1 === 1 / v2;
+    }
+    if (v1 !== v1) {
+      return v2 !== v2;
+    }
+    return v1 === v2;
+  };
+}
+
+//rodabase
 module.exports = require('./lib/roda');
